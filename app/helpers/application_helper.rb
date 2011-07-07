452392676746de1a70b2ea8b75cccdf68d4b7230
Hugo Villero <hugo.villero@gmail.com>
2011-07-06 20:04:49 -0400

    layout with css for the app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
-module ApplicationHelper
+module ApplicationHelper           
+  
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if not @title   
+       base_title
+     else
+       "#{base_title} | #{@title}"  
+    end
+          
+  end
+    
 end
